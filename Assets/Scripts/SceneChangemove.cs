cdc61719a86c9ef0f5cf85b334f59eef760a42c2
vwjames <vwjames@sfu.ca>
2023-06-07 15:57:02 -0700

    Added watering can to files

--- Assets/Scripts/SceneChangemove.cs
+++ Assets/Scripts/SceneChangemove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class SceneChange : MonoBehaviour
+public class SceneChangeMove : MonoBehaviour
 {
   public string[] sceneNames; // Array of scene names to load
 
